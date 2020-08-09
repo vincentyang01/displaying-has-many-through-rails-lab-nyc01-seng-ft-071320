acd3c892f17f38cbe485b5443efea95cd0eaffc2
vincentyang01 <vincentyangcsi@gmail.com>
2020-08-09 01:52:13 -0400

    Done.

---
+++ db/migrate/20200809041016_create_appointments.rb
@@ -0,0 +1,14 @@
+class CreateAppointments < ActiveRecord::Migration[5.0]
+  def change
+    create_table :appointments do |t|
+      t.integer :doctor_id
+      t.integer :patient_id
+      t.datetime :appointment_datetime
+
+      t.belongs_to :doctor
+      t.belongs_to :patient
+
+      t.timestamps
+    end
+  end
+end
